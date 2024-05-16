7644cae56e52630749afb71d81090eff1190ed22
Nicolas M <160145620+nmarzagaodev@users.noreply.github.com>
2024-05-16 10:12:45 -0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,10 @@
+const List = require('./list.js');
+const ListNode = require('./list.js');
+
+const mylist = new List()
+
+mylist.push_back(new ListNode("First"));
+
+console.log(mylist.is_empty())
+
+
